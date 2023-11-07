9d8c6d5795bfa73b26a0adfc646a2d6b47c3bb9b
onurkaymak <onurkaymak@outlook.com>
2023-11-07 15:13:21 -0800

    Pass Test #1 for tje storeState function.

--- src/js/plant.js
+++ src/js/plant.js
@@ -6,3 +6,12 @@ export const changeState = (prop) => {
     })
   }
 }
+
+export const storeState = () => {
+  let currentState = { soil: 0, light: 0, water: 0 };
+  return (stateChangeFunction) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = { ...newState };
+    return newState;
+  }
+}
