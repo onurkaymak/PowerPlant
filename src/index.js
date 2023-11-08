bf6144955b3a9e7d15fdded80a00231a5e6ca0ef
onurkaymak <onurkaymak@outlook.com>
2023-11-07 16:18:42 -0800

    Add UI logic.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-import { testFunc } from './js/business';
-import './css/styles.css';
+import { changeState, storeState } from './js/plant';
+
+const stateControl = storeState();
+
+const feed = changeState("soil")(1);
+const blueFood = changeState("soil")(5);
+
+const hydrate = changeState("water")(1);
+const superWater = changeState("water")(5);
 
 
-window.addEventListener('load', testFunc);
+window.onload = function () {
+  document.getElementById('feed').onclick = function () {
+    const newState = stateControl(blueFood);
+    document.getElementById('soil-value').innerText = `Soil: ${newState.soil}`;
+  };
+
+  document.getElementById('show-state').onclick = function () {
+    const currentState = stateControl();
+    document.getElementById('soil-value').innerText = `Soil: ${currentState.soil}`;
+  };
+};
